fac2def7668ae86ce3a2b477e36bf8f9a8fb8035
Ushna Patel <75239579+Ushan1281@users.noreply.github.com>
2022-03-19 14:32:39 +0530

    Create backend.php

---
+++ backend.php
@@ -0,0 +1,30 @@
+<?php
+    $server ="localhost";
+    $username = "root";
+    $password = "";
+
+    $con = mysqli_connect($server, $username, $password);
+
+    $name = $_POST['name'];
+    $age = $_POST['age'];
+    $gender = $_POST['gender'];
+    $email = $_POST['email'];
+    $pnone = $_POST['phone'];
+    $desc = $_POST['desc'];
+
+    $sql = "INSERT INTO `firstwebpg`.`tripdata` (`name`, `age`, `gender`, `email`, `phone`, `desc`, `date`) 
+    VALUES ('$name', '$age', '$gender', '$email', '$pnone', '$desc', current_timestamp());";
+    //echo "$sql";
+        if($con->query($sql) == true){
+        //echo "Successfully inserted";
+        //  $insert = true;
+            }
+        else{
+        echo "ERROR: $sql <br> $con->error";
+            
+            }
+            
+        $con->close();
+ 
+
+?>
